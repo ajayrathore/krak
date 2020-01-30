bfc41d43e6edcffac1c25d1cafe1546b3b3418f6
Ajay Rathore <rathore.ajay@gmail.com>
2020-01-30 00:33:29 +0000

    Schema Registry Integration and use rust kafka

--- src/publisher.rs
+++ src/publisher.rs
@@ -1,24 +1,26 @@
 use crate::encoder::*;
 use rkdb::{kbindings::*, types::*};
 use kafka::producer::{Producer, Record, RequiredAcks};
-use lazy_static;
 use std::time::Duration;
-use crate::schema::Trade;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
+lazy_static! {
+    static ref PRODUCER : Mutex<Producer> = Mutex::new(Producer::from_hosts(vec!("localhost:9092".to_owned()))
+        .with_ack_timeout(Duration::from_secs(1))
+        .with_required_acks(RequiredAcks::One)
+        .create()
+        .unwrap());
+}
 
 #[no_mangle]
 pub extern "C" fn publish(tbl: *const K, rows: *const K) -> *const K {
     let payload = encode_table(tbl, rows);
-    let mut producer = Producer::from_hosts(vec!("localhost:9092".to_owned()))
-        .with_ack_timeout(Duration::from_secs(1))
-        .with_required_acks(RequiredAcks::One)
-        .create()
-        .unwrap();
     let mut records = Vec::new();
     for record in payload{
         records.push(Record::from_value("trades", record));
     }
-    let result = producer.send_all(&records).unwrap();
+    let result = PRODUCER.lock().unwrap().send_all(&records).unwrap();
     println!("publish status = {:?}", result);
     kvoid()
 }
