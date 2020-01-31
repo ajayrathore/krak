529ff9c2911498a8e019bd12fa10e8ed8e681f9d
Ajay Rathore <rathore.ajay@gmail.com>
2020-01-31 20:06:15 +0000

    Schema Registry Integration and use rust kafka

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 extern crate avro_rs;
 extern crate rkdb;
 
-pub mod schema;
 pub mod encoder;
 pub mod publisher;
 pub mod receiver;
@@ -12,11 +11,22 @@ pub mod receiver;
 
 mod tests {
     use schema_registry_converter::schema_registry::{SuppliedSchema, post_schema};
-    use crate::schema::RAW_SCHEMA;
     use failure::_core::fmt::Error;
 
     #[test]
     fn test_post_schema_to_registry() -> Result<(), Error> {
+        let RAW_SCHEMA: &str = r#"
+            {
+                "type": "record",
+                "name": "trade",
+                "fields": [
+                    {"name": "sid", "type": "int"},
+                    {"name": "sym", "type": "string"},
+                    {"name": "price", "type": "double"},
+                    {"name": "size", "type": "long"}
+                ]
+            }
+        "#;
         let trade_schema = SuppliedSchema::new(String::from(RAW_SCHEMA));
         let result = post_schema("localhost:8081/subjects/trade-value/versions", trade_schema);
         println!("Received Id: {:?}", result);
