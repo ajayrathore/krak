656f08339a03e5e1d09aba7b6e453a6db7c5f519
Ajay Rathore <rathore.ajay@gmail.com>
2020-02-02 23:59:01 +0000

    Added key parameter to publisher

--- src/publisher.rs
+++ src/publisher.rs
@@ -6,6 +6,7 @@ use std::time::Duration;
 use lazy_static::lazy_static;
 use std::sync::Mutex;
 use schema_registry_converter::schema_registry;
+use failure::_core::cell::RefCell;
 
 
 lazy_static! {
@@ -18,11 +19,17 @@ lazy_static! {
 
 
 #[no_mangle]
-pub extern "C" fn publish(tbl: *const K, rows: *const K, colnames: *const K) -> *const K {
+pub extern "C" fn publish(msg_key: *const K, tbl: *const K, rows: *const K, colnames: *const K) -> *const K {
     let payload = encode_table(tbl, rows, colnames);
+
+    let raw_key : RefCell<Vec<u8>> = RefCell::new(Vec::new());
+    if let KVal::String(key) = KVal::new(msg_key) {
+        raw_key.borrow_mut().clone_from(&key.as_bytes().to_vec());
+    }else { println!("Key should be string"); return kvoid()};
+
     let mut records = Vec::new();
     for record in payload{
-        records.push(Record::from_value("trades", record));
+        records.push(Record::from_key_value("trades", raw_key.clone().into_inner(),record));
     }
     let result = PRODUCER.lock().unwrap().send_all(&records).unwrap();
     println!("publish status = {:?}", result);
